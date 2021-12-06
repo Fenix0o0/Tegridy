0f75d6290ba1bd7bf732008811d2e58fc8ab5bf5
Fenix0o0 <fenix.no.pro@gmail.com>
2021-12-06 14:16:54 +0100

    Logowanie naprawa

--- php/logowanie.php
+++ php/logowanie.php
@@ -6,16 +6,20 @@ $haslo = $_POST['passwd'];
 if(!empty($login)){
     $sql = "SELECT id, login, haslo from klienci where login='$login'";
     $result = mysqli_query($connect, $sql);
-        while($dane = mysqli_fetch_assoc($result)){
-                if($dane['haslo'] == md5($haslo)){
-                    $_SESSION['klient'] = $dane['id'];
-                    header("location:../sklep.php");
+    $dane = mysqli_fetch_assoc($result);
+    if($dane != NULL){
+        if($dane['haslo'] == md5($haslo)){
+        $_SESSION['klient'] = $dane['id'];
+        header("location:../sklep.php");
                     
-                }else{
-                    header("location:../index.php");
-                    $_SESSION["Err"] = 'Błędne hasło lub login!';
-                }
+        }else{
+        header("location:../index.php");
+        $_SESSION["Err"] = 'Błędne hasło lub login!';
         }
+    }else{
+        header("location:../index.php");
+        $_SESSION["Err"] = 'Podane konto nie istnieje';
+    }
 }else{
     header("location:../index.php");
     $_SESSION["Err"] = "Błędny login!";
